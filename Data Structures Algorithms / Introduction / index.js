414d712d13fab822cf684a1eaeeb6f1388d9a57f
Ayaz Shaik <148182687+shaikayaz9@users.noreply.github.com>
2024-09-23 20:46:47 +0530

    Update  index.js

--- Data Structures Algorithms / Introduction / index.js
+++ Data Structures Algorithms / Introduction / index.js
@@ -1,2 +1,16 @@
 // data structures and algorithms in javascript.
 
+
+// Data Structure 👇
+const studentsDatabase = ["jordan", "erick", "john", "michel"];
+
+// Algorithm for finding a specific user
+const findStudent = (allStudents, studentName) => {
+  for (let i = 0; i < allStudents.length; i++) {
+    if (allStudents[i] === studentName) {
+      console.log(`Found ${studentName}`);
+    }
+  }
+};
+
+findStudent(studentsDatabase, "erick");
