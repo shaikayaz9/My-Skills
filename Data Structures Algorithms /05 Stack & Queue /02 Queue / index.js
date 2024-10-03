9de8ce8214aee46818b69b9d7a0304c1ea100054
Ayaz Shaik <148182687+shaikayaz9@users.noreply.github.com>
2024-10-03 18:42:40 +0530

    Update 05 Stack & Queue

---
+++ Data Structures Algorithms /05 Stack & Queue /02 Queue / index.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+
+    this.last.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    let temp = this.first;
+
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    }
